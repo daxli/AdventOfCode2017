8850faae722a2ccef38ea4f49de19e97be8ce6b1
DomePapa <demeter.333@gmail.com>
2017-12-02 10:29:24 +0100

    part 2 solved

--- Day02_CorruptionChecksum/solution.py
+++ Day02_CorruptionChecksum/solution.py
@@ -1,11 +1,24 @@
 #------------------------------------------------------------------------------#
-#  Part 1 
+#  Part 1
 def partOne(data):
     sum = 0
     for line in data.split('\n'):
         orderedLine = sorted(map(lambda x: int(x), line.split('\t')))
         difference = abs(orderedLine[0] - orderedLine[-1])
         sum = sum + difference
+    return sum
+
+#------------------------------------------------------------------------------#
+#  Part 2
+def partTwo(data):
+    sum = 0
+    for line in data.split('\n'):
+        orderedLine = map(lambda x: int(x), line.split('\t'))
+        for dividend in orderedLine:
+            for divider in orderedLine:
+                if (dividend != divider) and not (dividend % divider):
+                    sum = sum + (dividend / divider)
+                    break
     return sum
 
 #------------------------------------------------------------------------------#
@@ -31,10 +44,18 @@ if __name__ == "__main__":
     # Test Part1 solution
     partOneSecarios = { 
         '5\t1\t9\t5': 8,
-        '7\t5\t3': 4,
+        '7\t5\t3':    4,
         '2\t4\t6\t8': 6
     }
     tester(partOne, partOneSecarios)
+
+    # Test Part1 solution
+    partTwoSecarios = { 
+        '5\t9\t2\t8': 4,
+        '9\t4\t7\t3': 3,
+        '3\t8\t6\t5': 2
+    }
+    tester(partTwo, partTwoSecarios)
 
     #  Read input file
     with open('input.txt', 'r') as file:
@@ -42,3 +63,4 @@ if __name__ == "__main__":
     
     print 'Day02: Corruption Checksum'
     print '\tPart 1 solution: {0}'.format(partOne(inputData))
+    print '\tPart 2 solution: {0}'.format(partTwo(inputData))
